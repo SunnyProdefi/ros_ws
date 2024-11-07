80c4d1181ad86a5431e1cc54874e479ef0e76bd5
sunnyprodefi <sunprodefi@gmail.com>
2024-11-07 15:27:04 +0800

    ch2-last

---
+++ src/communicate/src/turtlesim_srv.cpp
@@ -0,0 +1,25 @@
+#include "ros/ros.h"
+#include "turtlesim/Spawn.h"
+
+int main(int argc, char *argv[]) {
+  setlocale(LC_ALL, "");
+
+  ros::init(argc, argv, "set_turtle");
+  ros::NodeHandle nh;
+  ros::ServiceClient client = nh.serviceClient<turtlesim::Spawn>("/spawn");
+
+  ros::service::waitForService("/spawn");
+
+  turtlesim::Spawn spawn;
+  spawn.request.x = 1.0;
+  spawn.request.y = 1.0;
+  spawn.request.theta = 1.57;
+  spawn.request.name = "my_turtle";
+
+  bool flag = client.call(spawn);
+  if (flag) {
+    ROS_INFO("新的乌龟生成,名字:%s", spawn.response.name.c_str());
+  } else {
+    ROS_INFO("乌龟生成失败！！！");
+  }
+}
